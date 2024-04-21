ef3be1aa6e78adba7dd878b4cf5d1729dc4ffcfb
lambiengcode <lambiengcode@gmail.com>
2024-04-21 09:00:46 +0700

    update .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,23 +16,23 @@ module.exports = {
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       kill_timeout: 30000,
     },
-    {
-      name: 'waterbus.sfu.ws.02',
-      script: 'dist/src/main.js',
-      instances: 1, // can set is "max" for using max of processors
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '2G',
-      exec_mode: 'fork',
-      env: {
-        NODE_ENV: 'development',
-        PORT: 5986,
-        WEBSOCKET_GRPC_ADDRESS: '0.0.0.0:50057',
-      },
-      error_file: 'logs/error.log',
-      out_file: 'logs/out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      kill_timeout: 30000,
-    },
+    // {
+    //   name: 'waterbus.sfu.ws.02',
+    //   script: 'dist/src/main.js',
+    //   instances: 1, // can set is "max" for using max of processors
+    //   autorestart: true,
+    //   watch: false,
+    //   max_memory_restart: '2G',
+    //   exec_mode: 'fork',
+    //   env: {
+    //     NODE_ENV: 'development',
+    //     PORT: 5986,
+    //     WEBSOCKET_GRPC_ADDRESS: '0.0.0.0:50057',
+    //   },
+    //   error_file: 'logs/error.log',
+    //   out_file: 'logs/out.log',
+    //   log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    //   kill_timeout: 30000,
+    // },
   ],
 };
